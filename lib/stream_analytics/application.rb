e58f9ed467afe701a4422c2ea6ec98a6f4f9a481
Nasita Haque <nasita.haque@gmail.com>
2017-08-08 10:05:21 -0400

    add correct comment graph

--- lib/stream_analytics/application.rb
+++ lib/stream_analytics/application.rb
@@ -34,15 +34,15 @@ module StreamAnalytics
       messages = []
       api_params = { liveChatId: params[:live_chat_id], part: 'id, snippet, authorDetails' }
 
-      loop do
-        break if page_count == 5
-
-        if next_page_token && !next_page_token.empty?
-          api_params[:pageToken] = next_page_token
-        end
+      # loop do
+      #   break if page_count == 5
+      #
+      #   if next_page_token && !next_page_token.empty?
+      #     api_params[:pageToken] = next_page_token
+      #   end
 
         messages_api = youtube_api('liveChat/messages', api_params)
-        break if messages_api['pageInfo']['totalResults'] == 0
+        # break if messages_api['pageInfo']['totalResults'] == 0
 
         messages = messages_api['items'].map do |message|
           {
@@ -52,22 +52,39 @@ module StreamAnalytics
           }
         end.concat(messages)
 
-        next_page_token = messages_api['nextPageToken']
-        break if !next_page_token || next_page_token.empty?
-        puts "NPT: #{next_page_token}"
-
-        puts "ENTERING SLEEP #{messages_api['pollingIntervalMillis']}"
-        sleep (messages_api['pollingIntervalMillis'] / 100)
-        puts "EXITING SLEEP"
-
-        page_count += 1
-      end
+        # next_page_token = messages_api['nextPageToken']
+        # break if !next_page_token || next_page_token.empty?
+        # puts "NPT: #{next_page_token}"
+        #
+        # puts "ENTERING SLEEP #{messages_api['pollingIntervalMillis']}"
+        # sleep (messages_api['pollingIntervalMillis'] / 100)
+        # puts "EXITING SLEEP"
+        #
+        # page_count += 1
+      # end
 
       users = messages.each_with_object(Hash.new(0)) do |message, counter|
         counter[message[:author]] += 1
       end.sort_by { |name, count| count }
         .reverse.map do |name, count|
         { content: name, count: count }
+      end
+
+      comments_per_s = messages.each_with_object(Hash.new(0)) do |message, counter|
+      t = Time.parse(message[:timestamp])
+      # rounded_t = t-t.sec #if you just want to round to remove the seconds
+
+      # rounded_t = t-t.sec-t.min%1*60 #if you want to round to nearest minute
+      nearest = 15
+      rounded_t = t - t.sec%nearest
+      rounded_t = rounded_t.to_s
+      puts rounded_t
+      puts counter[rounded_t]
+      counter[rounded_t] +=1
+      end.map do |time, count|
+        {
+          timestamp: time, count:count
+        }
       end
 
       words = messages
@@ -80,7 +97,8 @@ module StreamAnalytics
       json({
         messages: messages,
         users: users,
-        words: words
+        words: words,
+        comments_per_s: comments_per_s
       })
     end
 
